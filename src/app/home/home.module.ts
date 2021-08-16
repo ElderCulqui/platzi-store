bec540d35193b74fb3bbd44639d46948df205217
ElderCulqui <elderculquig@gmail.com>
2021-08-15 23:44:09 -0500

    cambios

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BannerComponent } from './components/banner/banner.component';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+import { SharedModule } from '../shared/shared.module';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 'auto'
+};
+
+@NgModule({
+  declarations : [
+    BannerComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    SwiperModule,
+    SharedModule
+  ],
+  providers: [
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG
+    }
+  ],
+})
+export class HomeModule {
+
+}
